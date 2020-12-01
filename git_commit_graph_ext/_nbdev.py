c254f41bf72dc9b3412f2773b8c86584169896b6
Jakub Narębski <jnareb@gmail.com>
2020-12-01 21:57:02 +0100

    03_example_graphs.ipynb: Add further examples from FELINE paper
    Add example of "a small DAG" from Figure 2 in the FELINE paper as
    `small_DAG_FELINE()`, and "a didactic example of a DAG" (a tree DAG)
    from Figure 6 in the FELINE paper as `tree_DAG()`.
    
    NOTE: The `example_graph.py` was regenerated from the notebook.
    
    This concludes the list of example small graphs (DAGs) from the FELINE
    paper:
      Renê R. Veloso, Loïc Cerf, Wagner Meira Jr, Mohammed J. Zaki:
      "Reachability Queries in Very Large Graphs:
       A Fast Refined Online Search Approach",
      Proc. 17th International Conference on Extending Database Technology (EDBT),
      March 24-28, 2014, Athens, Greece: ISBN 978-3-89318065-3,
      http://openprocedings.org/EDBT/2014/paper_166.pdf

--- git_commit_graph_ext/_nbdev.py
+++ git_commit_graph_ext/_nbdev.py
@@ -2,7 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"crown_DAG": "03_example_graphs.ipynb"}
+index = {"crown_DAG": "03_example_graphs.ipynb",
+         "small_DAG_FELINE": "03_example_graphs.ipynb",
+         "tree_DAG": "03_example_graphs.ipynb"}
 
 modules = ["example_graphs.py"]
 
