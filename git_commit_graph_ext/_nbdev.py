45da683a69e87857a302de6d755e5db2039c2673
Jakub Narębski <jnareb@gmail.com>
2020-12-04 01:50:49 +0100

    03_example_graphs.ipynb: Add graph from Fig. 9 in FELINE
    Figure 9 in the FELINE paper is used to help explain the applicability
    of level filter.  The example shows the case with a false-positive for
    FELINE label, but which can be pruned in constant time with topological
    levels - for query $r(h, g)$.
    
    This graph is added as `levels_DAG_FELINE()` to the `example_graphs`
    module.
    
    While at it, the `matplotlib.pyplot` module is needed only for
    demonstration purposes, so don't export it.

--- git_commit_graph_ext/_nbdev.py
+++ git_commit_graph_ext/_nbdev.py
@@ -5,6 +5,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"crown_DAG": "03_example_graphs.ipynb",
          "small_DAG_FELINE": "03_example_graphs.ipynb",
          "tree_DAG": "03_example_graphs.ipynb",
+         "levels_DAG_FELINE": "03_example_graphs.ipynb",
          "RCH_graph": "03_example_graphs.ipynb",
          "commit_graph": "03_example_graphs.ipynb"}
 
