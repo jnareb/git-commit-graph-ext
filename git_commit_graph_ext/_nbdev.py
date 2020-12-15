6f5a6f6f6feebebb91c4880aafc41c16d9d86e3d
Jakub Narębski <jnareb@gmail.com>
2020-12-15 21:25:47 +0100

    09_git.ipynb: Code for getting commit graph of Git repos
    Add functions for cloning git repositories for extracting the commit
    graph, extracting the commit graph into adjacency list file format, and
    for getting the commit graph of the project history as NetworkX
    `DiGraph` (directed graph).
    
    TODO: add tests in the notebook for those new functions.
    
    Code entered and formatted using PyCharm Professional 2020.3 + Kite
    (nbdev_build_lib -> edit -> nbdev_update_lib -> nbdev_build_lib).

--- git_commit_graph_ext/_nbdev.py
+++ git_commit_graph_ext/_nbdev.py
@@ -7,7 +7,7 @@ index = {"crown_DAG": "03_example_graphs.ipynb",
          "tree_DAG": "03_example_graphs.ipynb",
          "levels_DAG_FELINE": "03_example_graphs.ipynb",
          "RCH_graph": "03_example_graphs.ipynb",
-         "commit_graph": "03_example_graphs.ipynb",
+         "commit_graph": "09_git.ipynb",
          "find_levels": "06_levels.ipynb",
          "find_dfs_spanning": "07_interval_labels.ipynb",
          "find_dfs_intervals": "07_interval_labels.ipynb",
@@ -17,12 +17,18 @@ index = {"crown_DAG": "03_example_graphs.ipynb",
          "reachable_positive_cut": "08_reach.ipynb",
          "reachable_negative_cut": "08_reach.ipynb",
          "walk_spanning": "08_reach.ipynb",
-         "generic_is_reachable_bfs": "08_reach.ipynb"}
+         "generic_is_reachable_bfs": "08_reach.ipynb",
+         "sparse_clone": "09_git.ipynb",
+         "get_repo": "09_git.ipynb",
+         "repo_generate_adjlist": "09_git.ipynb",
+         "repo_adjlist_to_graph": "09_git.ipynb",
+         "repo_to_graph": "09_git.ipynb"}
 
 modules = ["example_graphs.py",
            "labelling/levels.py",
            "labelling/dfs_intervals.py",
-           "reachability.py"]
+           "reachability.py",
+           "commit_graph.py"]
 
 doc_url = "https://jnareb.github.io/git-commit-graph-ext/"
 
